24ed4fd502110ed94f597fec1ebfe8f5d72c8a8f
Varun Yadav <yadavvarunrai2002@gmail.com>
2025-09-24 21:06:15 +0530

    Time: 51 ms (26.95%) | Memory: 115 MB (43.82%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class LRUCache {
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private int capacity;
+    private Map<Integer, Node> map;
+    private Node head, tail; // dummy head & tail
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        // dummy head & tail for easy operations
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+
+        Node node = map.get(key);
+        remove(node);
+        insertToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            // remove old node
+            remove(map.get(key));
+        } else if (map.size() == capacity) {
+            // remove LRU node (tail.prev)
+            remove(tail.prev);
+        }
+
+        Node node = new Node(key, value);
+        insertToHead(node);
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertToHead(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
