960fdb78702fceed3c26e389ebda2ba9cac33507
Varun Yadav <yadavvarunrai2002@gmail.com>
2025-09-26 22:24:13 +0530

    Time: 6 ms (85.53%) | Memory: 42.8 MB (57.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n=s1.length();
-        int m=s2.length();
-        if(m<n)return false;
-        int arr1[]=new int[26];
-        
-        for(int i=0;i<n;i++){
-            arr1[s1.charAt(i)-'a']++;
+        int n = s1.length();
+        int m = s2.length();
+        if (m < n) return false;
+
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            arr1[s1.charAt(i) - 'a']++;
+            arr2[s2.charAt(i) - 'a']++;
         }
-        for(int i=0;i<=m-n;i++){
-            int arr2[]=new int[26];
-            for(int j=0;j<n;j++){
-                arr2[s2.charAt(i+j)-'a']++;
-            }
-            if(isMatched(arr1,arr2)){
-                return true;
-            }
+
+        if (isMatched(arr1, arr2)) return true;
+
+        for (int i = 1; i <= m - n; i++) {
+            arr2[s2.charAt(i - 1) - 'a']--;      // remove leftmost
+            arr2[s2.charAt(i + n - 1) - 'a']++;  // add new rightmost
+            if (isMatched(arr1, arr2)) return true;
         }
+
         return false;
     }
-    private boolean isMatched(int arr1[],int arr2[]){
-        for(int i=0;i<26;i++){
-            if(arr1[i]!=arr2[i])return false;
+
+    private boolean isMatched(int[] arr1, int[] arr2) {
+        for (int i = 0; i < 26; i++) {
+            if (arr1[i] != arr2[i]) return false;
         }
         return true;
     }
