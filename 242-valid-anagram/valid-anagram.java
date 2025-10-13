62e361532a94300b17a590d207196c2c352218eb
Varun Yadav <yadavvarunrai2002@gmail.com>
2025-10-13 19:12:26 +0530

    Time: 2 ms (97.64%) | Memory: 43.5 MB (63.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,26 +5,21 @@ class Solution {
         if (s.length() != t.length()) return false;
         int len=s.length();
         int freq[]=new int[26];
-       int indexS=0;
-       int indexT=0;
-       while(indexS<len && indexT<len){
-        char charS=s.charAt(indexS);
-        int freqIndexS=charS-97;
-        freq[freqIndexS]+=1;
-
-        char charT=t.charAt(indexT);
-        int freqIndexT=charT-97;
-        freq[freqIndexT]-=1;
-        indexS++;
-        indexT++;
-       }
-       for(int i=0;i<26;i++){
+      for(char c:s.toCharArray()){
+        freq[c-'a']++;
+      }
+      for(char c:t.toCharArray()){
+        if(freq[c-'a']>0){
+          freq[c-'a']--;
+        }
+       
+      }
+     for(int i=0;i<26;i++){
         if(freq[i]!=0){
             return false;
         }
-       }
-       return true;
-       
+     }
+     return true;
           
     }
 
