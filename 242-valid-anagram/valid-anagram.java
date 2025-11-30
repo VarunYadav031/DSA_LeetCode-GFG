ded22afdc371a47b73772ddfc43c4686e690cc8c
Varun Yadav <yadavvarunrai2002@gmail.com>
2025-11-30 22:35:12 +0530

    Time: 5 ms (57.78%) | Memory: 44.2 MB (70.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,27 +1,27 @@
-import java.util.*;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int len=s.length();
-        int freq[]=new int[26];
-      for(char c:s.toCharArray()){
-        freq[c-'a']++;
+      
+      //  if(s.length()!=t.length()){
+      //   return false;
+      //  }
+      //  char[] ch1=s.toCharArray();
+      //  char[] ch2=t.toCharArray();
+      //  Arrays.sort(ch1);
+      //  Arrays.sort(ch2);
+      //  if(Arrays.equals(ch1,ch2)){
+      //   return true;
+      //  }else{
+      //   return false;
+      //  }
+      if(s.length()!=t.length())return false;
+      int[]freq=new int[26];
+      for(int i=0;i<s.length();i++){
+        freq[s.charAt(i)-'a']++;
+        freq[t.charAt(i)-'a']--;
       }
-      for(char c:t.toCharArray()){
-        if(freq[c-'a']>0){
-          freq[c-'a']--;
-        }
-       
+      for(int count:freq){
+        if(count!=0)return false;
       }
-     for(int i=0;i<26;i++){
-        if(freq[i]!=0){
-            return false;
-        }
-     }
-     return true;
-          
+      return true;
     }
-
-   
 }
