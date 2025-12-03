d1625e5bc2a6edbb47607530ea7e280dca1ba332
Varun Yadav <yadavvarunrai2002@gmail.com>
2025-12-03 13:28:45 +0530

    Time: 531 ms (56.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,15 @@
+SELECT employee_id
+FROM Employees
+WHERE employee_id NOT IN (
+    SELECT employee_id FROM Salaries WHERE employee_id IS NOT NULL
+)
+
+UNION
+
+SELECT employee_id
+FROM Salaries
+WHERE employee_id NOT IN (
+    SELECT employee_id FROM Employees WHERE employee_id IS NOT NULL
+)
+
+ORDER BY employee_id asc;
