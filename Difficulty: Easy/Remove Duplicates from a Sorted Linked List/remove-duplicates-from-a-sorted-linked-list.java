904354fac015a414bc9f0a496e5da13c8e01b01c
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-02-17 17:32:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.java
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.java
@@ -10,15 +10,15 @@ class Node
 class Solution {
     // Function to remove duplicates from sorted linked list.
     Node removeDuplicates(Node head) {
-       if(head==null) return head;
-       Node curr=head;
-       while(curr!=null && curr.next!=null){
-           if(curr.data==curr.next.data){
-               curr.next=curr.next.next;
-           }else{
-               curr=curr.next;
-           }
-       }
-       return head;
+      if(head==null) return head;
+      Node curr=head;
+      while(curr!=null && curr.next!=null){
+          if(curr.data==curr.next.data){
+              curr.next=curr.next.next;
+          }else{
+              curr=curr.next;
+          }
+      }
+      return head;
     }
 }
