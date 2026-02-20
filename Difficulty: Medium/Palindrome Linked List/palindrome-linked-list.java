c41c55c4dbadeb1956e0c615552a898823057bfd
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-02-20 12:32:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -11,21 +11,38 @@ class Node {
 
 class Solution {
     public boolean isPalindrome(Node head) {
-        ArrayList<Integer> ans=new ArrayList<>();
-        Node curr=head;
-        while(curr!=null){
-            ans.add(curr.data);
-            curr=curr.next;
-        }
-        int i=0;
-        int j=ans.size()-1;
-        while(i<j){
-            if(!ans.get(i).equals(ans.get(j))){
+        if(head==null || head.next==null)return true;
+       //1st find middle of linkedList
+       Node midNode=findMid(head);
+       //2nd reverse the linkedlist
+       Node prev=null;
+       Node curr=midNode;
+       Node next;
+       while(curr!=null){
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+       }
+       //3rd check left half equal right half
+        Node right=prev;
+        Node left=head;
+        while(right!=null){
+            if(right.data!=left.data){
                 return false;
             }
-            i++;
-            j--;
+            right=right.next;
+            left=left.next;
         }
         return true;
+    }
+    public Node findMid(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
 }
