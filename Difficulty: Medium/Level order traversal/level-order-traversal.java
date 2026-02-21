6fd2aed7cb749cc0b7d29bc1e4ffa4297b31b8da
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-02-21 13:52:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Level order traversal/level-order-traversal.java
+++ Difficulty: Medium/Level order traversal/level-order-traversal.java
@@ -12,26 +12,24 @@ class Node {
 import java.util.*;
 class Solution {
     public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
-        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
-        if (root == null) return ans;
-        Queue<Node>q=new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size=q.size();
-            ArrayList<Integer>level=new ArrayList<>();
-            for(int i=0;i<size;i++){
-               Node curr= q.remove();
-               level.add(curr.data);
-                if(curr.left!=null){
-                    q.add(curr.left);
-                }
-                if(curr.right!=null){
-                    q.add(curr.right);
-                }
-            }
-            ans.add(level);
-        }
-        return ans;
-        
+      ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+      Queue<Node>q=new LinkedList<>();
+      q.add(root);
+      while(!q.isEmpty()){
+          int size=q.size();
+          ArrayList<Integer>level=new ArrayList<>();
+          for(int i=0;i<size;i++){
+              Node curr=q.remove();
+              level.add(curr.data);
+              if(curr.left!=null){
+                  q.add(curr.left);
+              }
+              if(curr.right!=null){
+                   q.add(curr.right);
+              }
+          }
+          ans.add(level);
+      }
+      return ans;
     }
 }
