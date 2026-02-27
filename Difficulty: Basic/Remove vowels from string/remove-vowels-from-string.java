ce8221eb516e9f68eee770e5918cd51c288cbda0
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-02-27 18:57:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+    String removeVowels(String s) {
+        StringBuilder sb=new StringBuilder();
+       for(int i=0;i<s.length();i++){
+           char ch=s.charAt(i);
+           if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+               continue;
+           }
+           sb.append(ch);
+           
+       }
+       return sb.toString(); 
+    }
+}
