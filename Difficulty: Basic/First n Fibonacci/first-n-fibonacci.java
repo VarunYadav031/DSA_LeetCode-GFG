d4148475a3360be1672393e230cc292d0210c2ec
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-03-10 00:00:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    // Function to return list containing first n fibonacci numbers.
+    public static int[] fibonacciNumbers(int n) {
+       int []arr=new int[n];
+       arr[0]=0;
+       if(n==1){
+           return new int[]{0};
+       }
+       arr[1]=1;
+       for(int i=2;i<n;i++){
+           arr[i]=arr[i-1]+arr[i-2];
+       }
+       return arr;
+    }
+}
