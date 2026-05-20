08b068344691c121b19925b9af89f700adaf7da7
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-05-21 00:12:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
@@ -12,23 +12,24 @@ class Node
 
 class Solution {
     Node sortedMerge(Node head1, Node head2) {
+        // code here
         Node dummy=new Node(-1);
-        Node current=dummy;
+        Node tail=dummy;
         while(head1!=null && head2!=null){
-            if(head1.data<=head2.data){
-                dummy.next=head1;
+            if(head1.data<head2.data){
+                tail.next=head1;
                 head1=head1.next;
             }else{
-                dummy.next=head2;
+                tail.next=head2;
                 head2=head2.next;
             }
-            dummy=dummy.next;
+            tail=tail.next;
         }
         if(head1!=null){
-          dummy.next=head1;  
+            tail.next=head1;
         }else{
-            dummy.next=head2;
+            tail.next=head2;
         }
-       return  current.next;
+        return dummy.next;
     }
 }
