0c2d6f8f33e27cc52ccc966d9a47325023569e60
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-05-23 11:35:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the Frequency/find-the-frequency.java
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.java
@@ -1,11 +1,11 @@
 class Solution {
     int findFrequency(int arr[], int x) {
-        int count=0;
+        int countfreq=0;
         for(int i=0;i<arr.length;i++){
-            if(x==arr[i]){
-                count++;
+            if(arr[i]==x){
+                countfreq +=1;
             }
         }
-        return count;
+       return  countfreq;
     }
 }
