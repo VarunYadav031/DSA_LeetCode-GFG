c7e00f4a924ebbc6b74caa956b3528efd067c730
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-05-27 16:58:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Intersection of two sorted arrays/intersection-of-two-sorted-arrays.java
+++ Difficulty: Easy/Intersection of two sorted arrays/intersection-of-two-sorted-arrays.java
@@ -1,31 +1,25 @@
-
-
 class Solution {
-    public static ArrayList<Integer> intersection(int[] arr1, int[] arr2) {
-        ArrayList<Integer> result = new ArrayList<>();
-        
-        int i = 0, j = 0;
-        
-        while (i < arr1.length && j < arr2.length) {
-            
-            if (arr1[i] == arr2[j]) {
-                
-                // Avoid duplicates in result
-                if (result.size() == 0 || result.get(result.size() - 1) != arr1[i]) {
-                    result.add(arr1[i]);
-                }
-                i++;
-                j++;
-                
-            } else if (arr1[i] < arr2[j]) {
-                i++;
-            } else {
-                j++;
+    public ArrayList<Integer> intersection(int arr1[], int arr2[]) {
+    ArrayList<Integer>ans=new ArrayList<>();
+    int i=0;
+    int j=0;
+    int n=arr1.length;
+    int m=arr2.length;
+    while(i<n && j<m){
+        if(arr1[i]==arr2[j]){
+            if(ans.size()==0 || ans.get(ans.size()-1)!=arr1[i]){
+                ans.add(arr1[i]);
             }
+            i++;
+            j++;
         }
-        return result;
+        else if(arr1[i]<arr2[j]){
+            i++;
+        }else{
+            j++;
+        }
+    }
+    return ans;
+        
     }
 }
-
-        
-    
