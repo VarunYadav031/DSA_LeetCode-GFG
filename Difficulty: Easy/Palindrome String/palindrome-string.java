df58d095adebd034c24b8a5458813dad282f2801
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-05-28 16:12:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.java
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -1,16 +1,15 @@
 class Solution {
    
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
             left++;
             right--;
         }
-
         return true;
     }
 
