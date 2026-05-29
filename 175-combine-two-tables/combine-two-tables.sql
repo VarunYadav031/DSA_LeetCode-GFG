55325b2410704d1a3adc19bb9d0d3ea42538c278
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-05-29 17:52:00 +0530

    Time: 466 ms (16.08%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select firstName,lastname,city,state
+select firstName,lastName,city,state
 from Person as p
-left join Address as a
-on  p.personId=a.personId
+left join 
+Address as a 
+on p.personId=a.personId 
