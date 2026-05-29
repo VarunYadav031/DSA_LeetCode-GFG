dd486b0ab90335cfe812e48ee3546f9f7f16cc13
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-05-29 16:38:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -5,18 +5,19 @@ class Solution {
         
         Stack<Integer> stack=new Stack<>();
         int n=arr.length;
-        for(int i=0;i<n;i++){
-            result.add(-1);
-        }
+        
         for(int i=n-1;i>=0;i--){
             while(!stack.isEmpty() && stack.peek()<=arr[i]){
                 stack.pop();
             }
-            if(!stack.isEmpty()){
-                result.set(i,stack.peek());
+            if(stack.isEmpty()){
+                result.add(-1);
+            }else{
+                result.add(stack.peek());
             }
             stack.push(arr[i]);
         }
+        Collections.reverse(result);
         return result;
     }
 }
