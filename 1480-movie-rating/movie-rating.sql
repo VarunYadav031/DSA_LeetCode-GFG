eceb743292435d7ae02e797bb772ec30b0cbf275
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-06-04 22:43:00 +0530

    Time: 1521 ms (17.10%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,20 +1,23 @@
 (
-SELECT u.name AS results
-FROM Users u
-JOIN MovieRating m ON u.user_id = m.user_id
-GROUP BY u.user_id, u.name
-ORDER BY COUNT(*) DESC, u.name ASC
-LIMIT 1
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr
+        ON u.user_id = mr.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY COUNT(*) DESC, u.name
+    LIMIT 1
 )
 
 UNION ALL
 
 (
-SELECT mv.title AS results
-FROM Movies mv
-JOIN MovieRating m ON mv.movie_id = m.movie_id
-WHERE DATE_FORMAT(m.created_at, '%Y-%m') = '2020-02'
-GROUP BY mv.movie_id, mv.title
-ORDER BY AVG(m.rating) DESC, mv.title ASC
-LIMIT 1
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr
+        ON m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01'
+      AND mr.created_at < '2020-03-01'
+    GROUP BY m.movie_id, m.title
+    ORDER BY AVG(mr.rating) DESC, m.title
+    LIMIT 1
 );
