a72ddd8c17094702863a95fa5293214ce0263d11
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-06-05 11:09:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Roman to Integer/roman-to-integer.java
+++ Difficulty: Easy/Roman to Integer/roman-to-integer.java
@@ -1,31 +1,36 @@
-import java.util.HashMap;
-
 class Solution {
     public int romanToDecimal(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('M', 1000);
-        map.put('D', 500);
-        map.put('C', 100);
-        map.put('L', 50);
-        map.put('X', 10);
-        map.put('V', 5);
-        map.put('I', 1);
-
-        int prev = 0;
-        int ans = 0;
-
-        for(int i = s.length() - 1; i >= 0; i--) {
-            int curr = map.get(s.charAt(i));
-
-            if(curr < prev) {
-                ans -= curr;
-            } else {
-                ans += curr;
+        int total=0;
+        int prev=0;
+        for(int i=s.length()-1;i>=0;i--){
+            int curr=getvalue(s.charAt(i));
+            if(curr<prev){
+                total-=curr;
+            }else{
+                total+=curr;
             }
-
-            prev = curr;
+            prev=curr;
         }
-
-        return ans;
+        return total;
+        
     }
+    public static int getvalue(char c){
+        if(c=='I'){
+            return 1;
+        }else if(c=='V'){
+            return 5;
+        }else if(c=='X'){
+            return 10;
+        }else if(c=='L'){
+            return 50;
+        }else if(c=='C'){
+            return 100;
+        }else if(c=='D'){
+            return 500;
+        }else {
+            return 1000;
+        }
+        
+    }
+    
 }
