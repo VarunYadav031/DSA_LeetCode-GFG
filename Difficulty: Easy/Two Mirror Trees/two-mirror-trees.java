4b07b56fc1d3693b0a57115c96452251c801a3ca
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-06-10 15:10:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Mirror Trees/two-mirror-trees.java
@@ -0,0 +1,22 @@
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    boolean areMirror(Node a, Node b) {
+        if(a==null && b==null) return true;
+        if(a==null || b==null) return false;
+        return(a.data==b.data)&&
+        areMirror(a.left,b.right)&&
+        areMirror(a.right,b.left);
+        
+    }
+}
