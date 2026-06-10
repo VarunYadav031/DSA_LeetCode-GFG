3ceb5bb532c64547a93929c6d2af1b226d400640
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-06-10 14:29:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.java
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.java
@@ -1,26 +1,24 @@
 /*
-// A Binary Tree node
-class Node
-{
+Definition for Node
+class Node {
     int data;
-    Node left, right;
+    Node left;
+    Node right;
 
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
     }
 }
 */
 
 class Solution {
-    // Function to return sum of all nodes of a binary tree
     static int sumBT(Node root) {
-       if(root==null) return 0;
-       int leftsum=sumBT(root.left);
-       int rightsum=sumBT(root.right);
-       int totalsum=leftsum+rightsum+root.data;
-       return totalsum;
         
+        if(root==null)return 0;
+       
+         return root.data +sumBT(root.left)+sumBT(root.right);
+       
     }
 }
