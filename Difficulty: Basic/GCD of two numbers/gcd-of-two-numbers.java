9619cb4f05cd8d60153977ab669b51aaf2e11f56
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-06-12 21:55:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.java
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.java
@@ -1,20 +1,7 @@
 class Solution {
     public static int gcd(int a, int b) {
-        // int min=Math.min(a,b);
-        // int result=1;       // incase of(7,8)
-        // for(int i=1;i<=min;i++){
-        //     if(a%i==0 && b%i==0){
-        //         result=i;
-        //     }
-        // }
-        // return result;
+        if(b==0)return a;
+        return gcd(b,a%b);
         
-        
-        while(b!=0){
-           int temp=b;
-           b=a%b;
-           a=temp;
-        }
-        return a;
     }
 }
