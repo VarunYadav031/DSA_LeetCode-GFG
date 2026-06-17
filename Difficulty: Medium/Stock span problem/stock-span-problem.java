b47bdd37617c68fded19696f223d3f46e1db60a3
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-06-17 10:32:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.java
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -1,24 +1,22 @@
 class Solution {
     public ArrayList<Integer> calculateSpan(int[] arr) {
-        ArrayList<Integer> span=new ArrayList<>();
-        Stack<Integer> stack=new Stack<>();
-        int n=arr.length;
-        span.add(1);
-        stack.push(0);
-        for(int i=1;i<arr.length;i++){
-            int currprice=arr[i];
-            while(!stack.isEmpty() && arr[stack.peek()]<=currprice){
-                stack.pop();
-            }
-            if(stack.isEmpty()){
-                span.add(i+1);
-            }else{
-                int prevhigh=stack.peek();
-                span.add(i-prevhigh);
-            }
-            stack.push(i);
-        
-        }
+       ArrayList<Integer>span=new ArrayList<>();
+       Stack<Integer>s=new Stack<>();
+       
+       
+       for(int i=0;i<arr.length;i++){
+           int currprice=arr[i];
+           while(!s.isEmpty() && currprice>=arr[s.peek()]){
+               s.pop();
+           }
+           if(s.isEmpty()){
+               span.add(i+1);
+           }else{
+               int prevhigh=s.peek();
+               span.add(i-prevhigh);
+           }
+           s.push(i);
+       }
         return span;
     }
 }
