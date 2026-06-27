7ce8a1b6149be5bb8f4752d0f9cef803ab99c4a4
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-06-27 11:01:44 +0530

    Time: 2 ms (36.51%) | Memory: 47.3 MB (37.65%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int[] col=new int[graph.length];
+        for(int i=0;i<graph.length;i++){
+            col[i]=-1;
+        
+        }
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<graph.length;i++){
+            if(col[i]==-1){
+                q.add(i);
+                col[i]=0;
+            }
+            while(!q.isEmpty()){
+               int curr=q.remove();
+               for(int j=0;j<graph[curr].length;j++){
+                int neigh=graph[curr][j];
+                if(col[neigh]==-1){
+                    int nextcol=(col[curr]==0)?1:0;
+                    col[neigh]=nextcol;
+                    q.add(neigh);
+                }else if(col[curr]==col[neigh]){
+                    return false;
+                }
+               }
+            }
+        }
+        return true;
+    }
+}
