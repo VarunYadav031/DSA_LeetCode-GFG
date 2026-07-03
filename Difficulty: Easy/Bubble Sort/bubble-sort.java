5f054ab866ad4d8089456119898c16c752c4aa62
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-07-03 19:23:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.java
+++ Difficulty: Easy/Bubble Sort/bubble-sort.java
@@ -6,7 +6,6 @@ class Solution {
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
-                
                 }
             }
         }
