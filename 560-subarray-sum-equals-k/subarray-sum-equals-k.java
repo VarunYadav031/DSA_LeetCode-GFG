cdffdd550853fbf7ad58c6962857be8c5760f1b7
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-08-12 17:32:03 +0530

    Time: 24 ms (76.14%) | Memory: 49 MB (22.71%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,26 +1,39 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n=nums.length;
-       int[]prefix=new int[n];
-       prefix[0]=nums[0];
-       for(int i=1;i<n;i++) {
-        prefix[i]=nums[i]+prefix[i-1];
-       }
-       int count=0;
-       for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            int sum=0;
-            if(i==0){
-            sum=prefix[j];
-            }
-            else{
-                sum=prefix[j]-prefix[i-1];
-            }
-             if(sum==k){
-                    count++;
-                }
+    //     int n=nums.length;
+    //    int[]prefix=new int[n];
+    //    prefix[0]=nums[0];
+    //    for(int i=1;i<n;i++) {
+    //     prefix[i]=nums[i]+prefix[i-1];
+    //    }
+    //    int count=0;
+    //    for(int i=0;i<n;i++){
+    //     for(int j=i;j<n;j++){
+    //         int sum=0;
+    //         if(i==0){
+    //         sum=prefix[j];
+    //         }
+    //         else{
+    //             sum=prefix[j]-prefix[i-1];
+    //         }
+    //          if(sum==k){
+    //                 count++;
+    //             }
+    //     }
+    //    }
+    //    return count;
+    HashMap<Integer,Integer>map=new HashMap<>();
+    map.put(0,1);
+    int count=0;
+    int sum=0;
+    for(int i=0;i<nums.length;i++){
+        sum +=nums[i];
+        if(map.containsKey(sum-k)){
+            count +=map.get(sum-k);
         }
-       }
-       return count;
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        
+    }
+    return count;
     }
 }
