a90abd31eab2964a9d9a1dca825fd893413b02f4
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-08-29 13:38:31 +0530

    Time: 1 ms (99.93%) | Memory: 45.2 MB (90.02%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int read=0;
+        int write=0;
+        while(read<chars.length){
+            char current=chars[read];
+            int count=0;
+            while(read<chars.length && chars[read]==current){
+                read++;
+                count++;
+            }
+            chars[write]=current;
+            write++;
+            if(count>1){
+                String countStr=String.valueOf(count);
+                for(char c:countStr.toCharArray()){
+                    chars[write]=c;
+                    write++;
+                }
+
+            }
+        }
+        return write;
+    }
+}
