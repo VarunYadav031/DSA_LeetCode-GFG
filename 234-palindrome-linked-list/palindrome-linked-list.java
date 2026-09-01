31014d9e2aa9d8faa335f324e5beedf26b559b9d
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-09-01 12:24:51 +0530

    Time: 4 ms (67.20%) | Memory: 94 MB (94.40%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,42 +10,36 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-    if(head==null || head.next==null){
-        return true;
-    }
-        //step.1 find mid
-       ListNode mid=findmid(head);
-        //step.2 reverse half of the LL
-        ListNode curr=mid;
-        ListNode prev=null;
-        ListNode next;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+        
+        if(head==null && head.next==null){
+            return true;
         }
-        ListNode right=prev;
-        ListNode left=head;
-        //step.3 check left half equal to right
-      while(right!=null){
-        if(right.val!=left.val){
-            return false;
-        }
-        left=left.next;
-        right=right.next;
-      }
-      return true;
-    }
-    
-    public ListNode findmid(ListNode head){
-       
-         ListNode slow=head;
-         ListNode fast=head;
+        ListNode slow=head;
+        ListNode fast=head;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        return slow;
+        ListNode second=reverse(slow);
+        ListNode first=head;
+        while(second!=null){
+           if(first.val!=second.val){
+            return false;
+           }
+           first=first.next;
+           second=second.next;
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode head){
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
     }
 }
