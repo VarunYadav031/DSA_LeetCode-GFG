5246b48e06b2579f2c2e789e5011acfb2eee3749
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-09-03 12:34:31 +0530

    Time: 1 ms (34.24%) | Memory: 45.6 MB (99.54%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,25 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
+    public int len(ListNode head){
+        int counter=0;
+        ListNode temp=head;
+        while(temp!=null){
+            counter++;
+            temp=temp.next;
+        }
+        return counter;
+    }
     public ListNode reverseKGroup(ListNode head, int k) {
-        // Step 1: check if there are at least k nodes
-        ListNode temp = head;
-        int cnt = 0;
-        while (cnt < k && temp != null) {
-            temp = temp.next;
-            cnt++;
+        int N=len(head);
+        int groups=N/k;
+        ListNode prevHead=null;
+        ListNode currHead=head;
+        ListNode ansnode=null;
+        for(int i=0;i<groups;i++){
+            ListNode curr=currHead;
+            ListNode prev=null;
+            ListNode next=null;
+            for(int j=0;j<k;j++){
+                next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+            }
+            if(prevHead==null){
+                ansnode=prev;
+            }else{
+              prevHead.next=prev;
+            }
+            prevHead=currHead;
+            currHead=curr;
         }
-        // if less than k nodes, return head (no reversal)
-        if (cnt < k) return head;
-        // Step 2: reverse k nodes
-        ListNode prev = reverseKGroup(temp, k); // recursive call for next groups
-        ListNode curr = head;
-        cnt = 0;
-        while (cnt < k) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-            cnt++;
-        }
-        return prev;
+        prevHead.next=currHead;
+        return ansnode;
     }
 }
