7089b2905e7a7ac7228d1bd2bdb3ad856fdbab06
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-09-23 22:07:15 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (96.85%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,46 +1,55 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution{
-    // Function to delete a node from BST.
-    public static TreeNode deleteNode(TreeNode root, int val) {
-        // code here.
-        if(root==null) return null;
-        if(val<root.val){
-            root.left=deleteNode(root.left,val);
-        }else if(val>root.val){
-            root.right=deleteNode(root.right,val);
-        }else{
-            if(root.left==null && root.right==null){
-                return null;
-            }else if(root.left==null){
-                return root.right;
-            }else if(root.right==null){
-                return root.left;
-            }else{
-                TreeNode successor=findMin(root.right);
-                root.val=successor.val;
-                root.right=deleteNode(root.right,successor.val);
-            }
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        // Tree empty or key not found
+        if (root == null) {
+            return null;
         }
+
+        // Key is in right subtree
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+
+        // Key is in left subtree
+        else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        }
+
+        // Key found
+        else {
+
+            // Case 1: No child
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // Case 2: Only right child
+            if (root.left == null) {
+                return root.right;
+            }
+
+            // Case 3: Only left child
+            if (root.right == null) {
+                return root.left;
+            }
+
+            // Case 4: Two children
+            TreeNode IS = findInorderSuccessor(root.right);
+
+            root.val = IS.val;
+
+            root.right = deleteNode(root.right, IS.val);
+        }
+
         return root;
     }
-    private static TreeNode findMin(TreeNode root){
-        while(root.left !=null){
-            root=root.left;
+
+    public static TreeNode findInorderSuccessor(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
         }
+
         return root;
     }
 }
